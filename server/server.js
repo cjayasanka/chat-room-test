7e3a9517e616e523a20282b92c7283f6582e1341
Chathura Jayasanka <chathura.15@cse.mrt.ac.lk>
2018-12-12 11:14:12 +0530

    connect and disconnect

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
 server.listen(3000, () => {
     console.log('server is up in port 3000');
 });
